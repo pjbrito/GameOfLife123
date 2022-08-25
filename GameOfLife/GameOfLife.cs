8f6077d6aa3be07d833710bc5b38d244094e94d7
David Foster <davidfoster1990@hotmail.co.uk>
2022-08-25 22:13:35 +0100

    implemented GameOfLife class with ctor, GetCurrentGrid and NextGen public methods; implemented 1 test for each public method

---
+++ GameOfLife/GameOfLife.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class GameOfLife
+    {
+        private int[,] grid;
+
+        public GameOfLife(int[,] grid)
+        {
+            this.grid = grid;
+        }
+
+        public int[,] GetCurrentGrid()
+        {
+            return grid;
+        }
+
+        public void NextGen()
+        {
+            if (grid == null)
+            {
+                throw new Exception("Grid must be defined");
+            }
+
+            int[,] nextGenGrid = grid.Clone() as int[,];
+
+            for (int x = 0; x < grid.GetLength(0); x++)
+            {
+                for (int y = 0; y < grid.GetLength(1); y++)
+                {
+                    if (LiveCell(grid[x,y]))
+                    {
+                        if (FewerThanTwoLiveNeighbours(grid, x, y))
+                        {
+                            nextGenGrid[x, y] = 0;
+                        }
+                        //else if (MoreThanThreeLiveNeighbours(grid[i, j]))
+                        //{
+                        //    nextGenGrid[i, j] = 0;
+                        //}
+                    }
+                    //else
+                    //{
+                    //    if (ExactlyThreeLiveNeighbours(grid[i, j]))
+                    //    {
+                    //        nextGenGrid[i, j] = 1;
+                    //    }
+                    //}
+                }
+            }
+
+            grid = nextGenGrid.Clone() as int[,];
+        }
+
+        private bool FewerThanTwoLiveNeighbours(int[,] grid, int xPosition, int yPosition)
+        {
+            int count = 0;
+            for (var x = xPosition -1; x < xPosition + 1; x++)
+            {
+                for (var y = yPosition - 1; y < yPosition + 1; y++)
+                {
+                    if(x >= 0 && y >= 0 && x < grid.GetLength(0) && y < grid.GetLength(1))
+                    {
+                        if (LiveCell(grid[x, y])) count++;
+                    }
+                }
+            }
+            return count < 2;
+        }
+
+        private bool LiveCell(int v)
+        {
+            return v == 1;
+        }
+    }
+}
--- GameOfLifeTests/RuleTests.cs
+++ GameOfLifeTests/RuleTests.cs
@@ -3,9 +3,56 @@ namespace GameOfLifeTests
     public class RuleTests
     {
         [Fact]
-        public void Test1()
+        public void GetCurrentGrid_BasicGridWith1LiveCellNoGenerations_ShouldReturnSameGrid()
         {
+            //arrange
+            int[,] startingGrid = new int[,]
+            {
+                { 0, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 0 }
+            };
 
+            //act
+            var gameOfLife = new GameOfLife.GameOfLife(startingGrid);
+            int[,] output = gameOfLife.GetCurrentGrid();
+
+            //assert
+            Assert.True(GridsAreEqual(startingGrid, output));
+        }
+
+        [Fact]
+        public void NextGen_LiveCellWithNoLiveNeighbours_ResultsInNoLiveCells()
+        {
+            //arrange
+            int[,] startingGrid = new int[,]
+            {
+                { 0, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 0 }
+            };
+
+            int[,] expectedGrid = new int[,]
+            {
+                { 0, 0, 0 },
+                { 0, 0, 0 },
+                { 0, 0, 0 }
+            };
+
+            //act
+            var gameOfLife = new GameOfLife.GameOfLife(startingGrid);
+            gameOfLife.NextGen();
+            int[,] output = gameOfLife.GetCurrentGrid();
+
+            //assert
+            Assert.True(GridsAreEqual(expectedGrid, output));
+        }
+
+        private bool GridsAreEqual(int[,] grid1, int[,] grid2)
+        {
+            return grid1.Rank == grid2.Rank &&
+                Enumerable.Range(0, grid1.Rank).All(dimension => grid1.GetLength(dimension) == grid2.GetLength(dimension)) &&
+                grid1.Cast<int>().SequenceEqual(grid2.Cast<int>());
         }
     }
 }
