d4f9d2c80b880bec0e5f60a4fe99995efb41a19c
PauloB <paulob@lendingmetrics.com>
2022-10-27 21:02:02 +0100

    added visualizer

--- GameOfLife/Program.cs
+++ GameOfLife/Program.cs
@@ -5,6 +5,22 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //var game = new int[,]
+            //{
+            //    { 0, 1, 0, 0 },
+            //    { 0, 1, 0, 0 },
+            //    { 0, 1, 0, 0 },
+            //    { 0, 1, 0, 0 }
+            //};
+
+            var game = new int[,]
+            {
+                { 0, 1, 0 },
+                { 0, 1, 0 },
+                { 0, 1, 0 },
+            };
+            (new VisualizeGameOfLive()).RunIterations(100, game);
         }
     }
 }
---
+++ GameOfLife/VisualizeGameOfLive.cs
@@ -0,0 +1,19 @@
+﻿namespace GameOfLife
+{
+    public class VisualizeGameOfLive
+    {
+        internal void RunIterations(int v, int[,] gameGrid)
+        {
+            var gol = new GameOfLife(gameGrid);
+            for (var i = 0; i < v; i++)
+            {
+                var cur = gol.GetCurrentGridVis();
+                Console.Clear();
+                Console.WriteLine($"{i}\n");
+                Console.WriteLine(cur);
+                Thread.Sleep(300);
+                gol.NextGen();
+            }
+        }
+    }
+}
